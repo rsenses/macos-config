51c087e1df626bc55d1bef841ac52a0c567f36f7
Rubén Silva <me@rsenses.com>
2023-03-31 18:35:36 +0200

    Anade ignore folders a telescope

--- nvim/lua/rsenses/remap.lua
+++ nvim/lua/rsenses/remap.lua
@@ -22,27 +22,27 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = '[P]aste over without yanking'})
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "[P]aste over without yanking"})
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = '[Y]ank to clipboard'})
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "[Y]ank to clipboard"})
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = '[D]elete without yanking'})
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "[D]elete without yanking"})
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[S]earch current word'})
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "[S]earch current word"})
 
 -- Telescope
-vim.keymap.set('n', '<leader>pf', require('telescope.builtin').find_files, { desc = '[F]ind files'})
-vim.keymap.set('n', '<leader>vh', require('telescope.builtin').help_tags, { desc = '[H]elp tags'})
-vim.keymap.set('n', '<leader>pg', require('telescope.builtin').live_grep, { desc = '[G]rep find'})
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[B]uffers find'})
+vim.keymap.set("n", "<leader>pf", require("telescope.builtin").find_files, { desc = "[F]ind files"})
+vim.keymap.set("n", "<leader>vh", require("telescope.builtin").help_tags, { desc = "[H]elp tags"})
+vim.keymap.set("n", "<leader>pg", require("telescope.builtin").live_grep, { desc = "[G]rep find"})
+vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[B]uffers find"})
 
 -- LSP
-vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, {})
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, {desc = "[G]oto [D]efinition"})
 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, {})
-vim.keymap.set("n", "<leader>pw", function() vim.lsp.buf.workspace_symbol() end, {desc = '[W]orkspace symbols'})
-vim.keymap.set("n", "<leader>ps", function() vim.lsp.buf.document_symbol() end, {desc = '[D]ocument symbols'})
-vim.keymap.set("n", "<leader>vd", '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>', {desc = '[D]iagnostics'})
-vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, {desc = '[F]ormat'})
+vim.keymap.set("n", "<leader>pw", function() vim.lsp.buf.workspace_symbol() end, {desc = "[W]orkspace symbols"})
+vim.keymap.set("n", "<leader>ps", function() vim.lsp.buf.document_symbol() end, {desc = "[D]ocument symbols"})
+vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, {desc = "[D]iagnostics"})
+vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, {desc = "[F]ormat"})
 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, {})
 vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, {})
