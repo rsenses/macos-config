4a0bb0fc3c7b7ad7c1e125d8fe3ff8644397f796
Rubén Silva <me@rsenses.com>
2023-04-02 12:58:54 +0200

    Mas refactorizacion

--- nvim/lua/rsenses/config/options.lua
+++ nvim/lua/rsenses/config/options.lua
@@ -1,4 +1,4 @@
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
@@ -17,14 +17,14 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 7
+vim.opt.sidescrolloff = 4
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
--- nvim/lua/rsenses/init.lua
+++ nvim/lua/rsenses/init.lua
@@ -1,22 +1,21 @@
 vim.g.mapleader = " "
 
-require("rsenses.config.options")
-
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup("rsenses.plugins")
+require("lazy").setup("rsenses.plugins")
 
+require("rsenses.config.options")
 require("rsenses.config.keymaps")
 require("rsenses.config.autocommands")
 require("rsenses.config.diagnostics")
