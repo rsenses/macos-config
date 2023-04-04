5fd210816877163676fc844f2daa7843c16c4a14
Rubén Silva <me@rsenses.com>
2023-04-04 14:23:27 +0200

    Elimina flechas en nvim para modo normal

--- nvim/lua/rsenses/config/keymaps.lua
+++ nvim/lua/rsenses/config/keymaps.lua
@@ -1,5 +1,10 @@
 local opts = { noremap = true, silent = true }
 
+vim.keymap.set("n", "<Up>", "<nop>", opts)
+vim.keymap.set("n", "<Down>", "<nop>", opts)
+vim.keymap.set("n", "<Left>", "<nop>", opts)
+vim.keymap.set("n", "<Right>", "<nop>", opts)
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
