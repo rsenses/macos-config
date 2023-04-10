d513a98654c11f87f4c5eddc951e71db6cb9b140
Rubén Silva <me@rsenses.com>
2023-04-10 13:06:15 +0200

    Anade ayudas visuales para las lineas

--- nvim/lua/rsenses/config/options.lua
+++ nvim/lua/rsenses/config/options.lua
@@ -32,6 +32,9 @@ vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 4
 vim.opt.signcolumn = "yes:2"
 vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.list = true -- show whitespace characters
+vim.opt.listchars:append('space:·') -- show tabs as »·
 
 vim.opt.updatetime = 4001 -- Set updatime to 1ms longer than the default to prevent polyglot from changing it
 vim.opt.redrawtime = 10000 -- Allow more time for loading syntax on large files
