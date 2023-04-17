feb55d51eabed676ba05a99025d3a6fb51d1dab5
Rubén Silva <me@rsenses.com>
2023-04-17 08:53:15 +0200

    Anade paddings a wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,8 +18,8 @@ config.line_height = 1.1
 config.audible_bell = "Disabled"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-	left = 0,
-	right = 0,
+	left = 2,
+	right = 2,
 	top = 2,
 	bottom = 0,
 }
