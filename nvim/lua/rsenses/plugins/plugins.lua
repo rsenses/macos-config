a1229a4252c0f2e857ebef634109471eba2f0e7f
Rubén Silva <me@rsenses.com>
2023-04-17 09:20:38 +0200

    Anade codeium y quita copilot, a ver que tal

--- nvim/lua/rsenses/plugins/plugins.lua
+++ nvim/lua/rsenses/plugins/plugins.lua
@@ -1,42 +1,61 @@
 return {
-    {
-        'lewis6991/gitsigns.nvim',
-        lazy = false,
-        dependencies = 'nvim-lua/plenary.nvim',
-        config = function()
-            require('gitsigns').setup({
-                sign_priority = 20,
-            })
-        end,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        lazy = true,
-        build = ":TSUpdate",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-            "nvim-treesitter/nvim-treesitter-context",
-            "lukas-reineke/indent-blankline.nvim",
-        },
-    },
-    {
-        "mbbill/undotree",
-        cmd = { "UndotreeShow", "UndotreeToggle", "UndotreeHide", "UndotreeFocus" },
-    },
-    {
-        "github/copilot.vim",
-        lazy = false,
-        config = function()
-            vim.g.copilot_no_tab_map = true
-            vim.g.copilot_assume_mapped = true
-        end
-    },
-    {
-        "tpope/vim-fugitive",
-        lazy = false,
-    },
-    {
-        "sheerun/vim-polyglot",
-        lazy = false,
-    },
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = false,
+		dependencies = "nvim-lua/plenary.nvim",
+		config = function()
+			require("gitsigns").setup({
+				sign_priority = 20,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = true,
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"nvim-treesitter/nvim-treesitter-context",
+			"lukas-reineke/indent-blankline.nvim",
+		},
+	},
+	{
+		"mbbill/undotree",
+		cmd = { "UndotreeShow", "UndotreeToggle", "UndotreeHide", "UndotreeFocus" },
+	},
+	-- {
+	-- 	"github/copilot.vim",
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 		vim.g.copilot_no_tab_map = true
+	-- 		vim.g.copilot_assume_mapped = true
+	-- 	end,
+	-- },
+	{
+		"exafunction/codeium.vim",
+		lazy = false,
+		config = function()
+			-- Change '<C-g>' here to any keycode you like.
+			vim.keymap.set("i", "<Tab>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true })
+			vim.keymap.set("i", "<c-n>", function()
+				return vim.fn["codeium#CycleCompletions"](1)
+			end, { expr = true })
+			vim.keymap.set("i", "<c-p>", function()
+				return vim.fn["codeium#CycleCompletions"](-1)
+			end, { expr = true })
+			vim.keymap.set("i", "<c-e>", function()
+				return vim.fn["codeium#Clear"]()
+			end, { expr = true })
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		lazy = false,
+	},
+	{
+		"sheerun/vim-polyglot",
+		lazy = false,
+	},
 }
