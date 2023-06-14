12c94d6e4f339cadfc9671055d57a36784ffb32c
Rubén Silva <me@rsenses.com>
2023-06-14 10:43:29 +0200

    codeuim accept ctrl + space

--- nvim/lua/rsenses/plugins/plugins.lua
+++ nvim/lua/rsenses/plugins/plugins.lua
@@ -36,16 +36,16 @@ return {
 		"exafunction/codeium.vim",
 		lazy = false,
 		config = function()
-			vim.keymap.set("i", "<a-space>", function()
+			vim.keymap.set("i", "<C-Space>", function()
 				return vim.fn["codeium#Accept"]()
 			end, { expr = true })
-			vim.keymap.set("i", "<c-n>", function()
+			vim.keymap.set("i", "<C-n>", function()
 				return vim.fn["codeium#CycleCompletions"](1)
 			end, { expr = true })
-			vim.keymap.set("i", "<c-p>", function()
+			vim.keymap.set("i", "<C-p>", function()
 				return vim.fn["codeium#CycleCompletions"](-1)
 			end, { expr = true })
-			vim.keymap.set("i", "<c-e>", function()
+			vim.keymap.set("i", "<C-e>", function()
 				return vim.fn["codeium#Clear"]()
 			end, { expr = true })
 		end,
