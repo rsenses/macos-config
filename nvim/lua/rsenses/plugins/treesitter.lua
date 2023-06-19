3c4b7397957e61a2076fd1b63b9a3b0c9ba56499
Rubén Silva <me@rsenses.com>
2023-06-19 16:16:37 +0200

    ligeras mejoras en kitty, tmux y nvim

--- nvim/lua/rsenses/plugins/treesitter.lua
+++ nvim/lua/rsenses/plugins/treesitter.lua
@@ -26,7 +26,7 @@ return {
 				-- Using this option may slow down your editor, and you may see some duplicate highlights.
 				-- Instead of true it can also be a list of languages
 				-- additional_vim_regex_highlighting = { "html" },
-				-- disable = { "html" },
+				disable = { "html" },
 			},
 			indent = {
 				enable = true,
