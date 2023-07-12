41814271536e58d4684f7f918419c1482b14af6d
Rubén Silva <me@rsenses.com>
2023-07-12 13:10:05 +0200

    Cambios en algunos plugins

--- nvim/lua/rsenses/plugins/plugins.lua
+++ nvim/lua/rsenses/plugins/plugins.lua
@@ -18,7 +18,26 @@ return {
 		dependencies = "nvim-treesitter/nvim-treesitter",
 		config = function()
 			require("nvim-ts-autotag").setup({
-				-- your config
+				filetypes = {
+					"html",
+					"javascript",
+					"typescript",
+					"javascriptreact",
+					"typescriptreact",
+					"svelte",
+					"vue",
+					"tsx",
+					"jsx",
+					"rescript",
+					"xml",
+					"php",
+					"markdown",
+					"astro",
+					"glimmer",
+					"handlebars",
+					"hbs",
+					"blade",
+				},
 			})
 		end,
 		lazy = true,
