e54e0919002551d8784a0be5fda6507e0d0eaf0a
Rubén Silva <me@rsenses.com>
2023-07-20 12:22:33 +0200

    Anade blade formatter y phpcsfixer

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -3,5 +3,7 @@ return {
   opts = function(_, opts)
     local nls = require("null-ls")
     table.insert(opts.sources, nls.builtins.formatting.prettier)
+    table.insert(opts.sources, nls.builtins.formatting.blade_formatter)
+    table.insert(opts.sources, nls.builtins.formatting.phpcsfixer)
   end,
 }
