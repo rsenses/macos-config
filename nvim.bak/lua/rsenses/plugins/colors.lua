9138a2a1eaeb465ced8c9cbfc1538613b6db44ac
Rubén Silva <me@rsenses.com>
2023-11-21 14:46:41 +0100

    Mas interlineado en kitty

--- nvim.bak/lua/rsenses/plugins/colors.lua
+++ nvim.bak/lua/rsenses/plugins/colors.lua
@@ -4,13 +4,13 @@ return {
 	name = "kanagawa",
 	config = function()
 		-- Kanagawa
-		vim.o.background = "dark"
+		-- vim.o.background = "dark"
 
 		require("kanagawa").setup({
 			transparent = true, -- do not set background color
 		})
 
 		-- setup must be called before loading
-		vim.cmd("colorscheme kanagawa")
+		vim.cmd("colorscheme kanagawa-wave")
 	end,
 }
