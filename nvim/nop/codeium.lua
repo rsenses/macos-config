b169da26f00cee92b9609ec1a6b0ecaa601bbeeb
Rubén Silva <me@rsenses.com>
2023-11-22 09:21:25 +0100

    Elimina varias configuraciones para hacer lazyvim mas standar

---
+++ nvim/nop/codeium.lua
@@ -0,0 +1,22 @@
+-- return {
+--   "exafunction/codeium.vim",
+--   cmd = "Codeium",
+--   build = ":Codeium Auth",
+--   lazy = false,
+--   config = function()
+--     vim.g.codeium_disable_bindings = 1
+--     vim.keymap.set("i", "<C-c>", function()
+--       return vim.fn["codeium#Accept"]()
+--     end, { expr = true })
+--     vim.keymap.set("i", "<C-n>", function()
+--       return vim.fn["codeium#CycleCompletions"](1)
+--     end, { expr = true })
+--     vim.keymap.set("i", "<C-p>", function()
+--       return vim.fn["codeium#CycleCompletions"](-1)
+--     end, { expr = true })
+--     vim.keymap.set("i", "<C-e>", function()
+--       return vim.fn["codeium#Clear"]()
+--     end, { expr = true })
+--     vim.g.codeium_no_map_tab = true
+--   end,
+-- }
