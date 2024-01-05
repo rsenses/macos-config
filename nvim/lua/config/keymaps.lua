88c7aecc169522aa2a6539640f8c25ddc9f0bc05
Rubén Silva <me@rsenses.com>
2024-01-05 08:54:50 +0100

    Anade mejoras en los keympas

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,3 +16,14 @@ vim.keymap.set("n", "<Leader>cf", "gg=G''", { desc = "Go to right window", remap
 -- Move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move lines down", remap = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move lines up", remap = true })
+-- Mejoras en los movimientos
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+-- Copiar, pegar y borrar sin usar el registro
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
