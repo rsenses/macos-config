49d1e2732698bcc3886c00a33d8fd7c0fad369e5
Rubén Silva <me@rsenses.com>
2024-01-05 09:44:24 +0100

    Instala undotree

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
 -- vim.keymap.del("n", "<C-h>")
 -- vim.keymap.del("n", "<C-j>")
 -- vim.keymap.del("n", "<C-k>")
@@ -11,8 +10,6 @@ vim.keymap.set("n", "<C-h>", ":<C-U>TmuxNavigateLeft<cr>", { desc = "Go to left 
 vim.keymap.set("n", "<C-j>", ":<C-U>TmuxNavigateDown<cr>", { desc = "Go to lower window", remap = true })
 vim.keymap.set("n", "<C-k>", ":<C-U>TmuxNavigateUp<cr>", { desc = "Go to upper window", remap = true })
 vim.keymap.set("n", "<C-l>", ":<C-U>TmuxNavigateRight<cr>", { desc = "Go to right window", remap = true })
--- Not LSP formatter
-vim.keymap.set("n", "<Leader>cf", "gg=G''", { desc = "Go to right window", remap = true })
 -- Move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move lines down", remap = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move lines up", remap = true })
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,4 +4,7 @@ return {
   opts = {},
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = { -- load the plugin only when using it's keybinding:
+    { "-", "<cmd>lua require('oil').open()<cr>", desc = "Open parent directory" },
+  },
 }
