f792dd33febdca11dee665c576ab9dabc245a8e1
Rubén Silva <me@rsenses.com>
2024-01-05 08:40:58 +0100

    Elimina algunos plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,8 +27,8 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
+        "matchit",
+        "matchparen",
         "netrwPlugin",
         "tarPlugin",
         "tohtml",
--- nvim/lua/plugins/disables.lua
+++ nvim/lua/plugins/disables.lua
@@ -3,4 +3,6 @@ return {
   { "folke/flash.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
 }
