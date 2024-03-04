faef2a79b11c99c64e7aef8672059deecb8a20af
Rubén Silva <me@rsenses.com>
2024-03-04 17:28:46 +0100

    optimiza conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,61 +1,23 @@
 local util = require("conform.util")
 return {
   "stevearc/conform.nvim",
-  opts = function()
-    ---@class ConformOpts
-    local opts = {
-      format = {
-        timeout_ms = 3000,
-        async = false, -- not recommended to change
-        quiet = false, -- not recommended to change
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        vue = { { "prettierd", "prettier" } },
-        css = { { "prettierd", "prettier" } },
-        scss = { { "prettierd", "prettier" } },
-        less = { { "prettierd", "prettier" } },
-        html = { { "prettierd", "prettier" } },
-        -- twig = { { "prettierd", "prettier" } },
-        json = { { "prettierd", "prettier" } },
-        jsonc = { { "prettierd", "prettier" } },
-        yaml = { { "prettierd", "prettier" } },
-        markdown = { { "prettierd", "prettier" } },
-        php = { "pint", "prettierd" },
-        blade = { "blade-formatter", "rustywind" },
-      },
-      -- LazyVim will merge the options you set here with builtin formatters.
-      -- You can also define any custom formatters here.
-      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-      formatters = {
-        injected = { options = { ignore_errors = true } },
-        -- # Example of using dprint only when a dprint.json file is present
-        -- dprint = {
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-        --   end,
-        -- },
-        --
-        -- # Example of using shfmt with extra args
-        -- shfmt = {
-        --   extra_args = { "-i", "2", "-ci" },
-        -- },
-        pint = {
-          meta = {
-            url = "https://github.com/laravel/pint",
-            description = "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
-          },
-          command = util.find_executable({
-            vim.fn.stdpath("data") .. "/mason/bin/pint",
-            "vendor/bin/pint",
-          }, "pint"),
-          args = { "$FILENAME" },
-          stdin = false,
-        },
-      },
-    }
-    return opts
-  end,
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { { "prettierd", "prettier" } },
+      typescript = { { "prettierd", "prettier" } },
+      vue = { { "prettierd", "prettier" } },
+      css = { { "prettierd", "prettier" } },
+      scss = { { "prettierd", "prettier" } },
+      less = { { "prettierd", "prettier" } },
+      html = { { "prettierd", "prettier" } },
+      -- twig = { { "prettierd", "prettier" } },
+      json = { { "prettierd", "prettier" } },
+      jsonc = { { "prettierd", "prettier" } },
+      yaml = { { "prettierd", "prettier" } },
+      markdown = { { "prettierd", "prettier" } },
+      php = { "pint" },
+      blade = { "blade-formatter", "rustywind" },
+    },
+  },
 }
