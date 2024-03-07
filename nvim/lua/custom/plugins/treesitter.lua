ad53c7d79893459d4b88fd29c0d74a73194265cf
Rubén Silva <me@rsenses.com>
2024-03-07 15:26:00 +0100

    Optimiza treesitter

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,149 +1,158 @@
 -- Highlight, edit, and navigate code
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  event = { 'BufRead', 'BufNewFile' },
-  dependencies = {
-    {
-      'EmranMR/tree-sitter-blade',
-      config = function()
-        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-        local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = { 'BufRead', 'BufNewFile' },
+    dependencies = {
+      {
+        'EmranMR/tree-sitter-blade',
+        config = function()
+          -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+          local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
 
-        parser_config.blade = {
-          install_info = {
-            url = 'https://github.com/EmranMR/tree-sitter-blade',
-            files = { 'src/parser.c' },
-            branch = 'main',
-          },
-          filetype = 'blade',
-        }
-      end,
+          parser_config.blade = {
+            install_info = {
+              url = 'https://github.com/EmranMR/tree-sitter-blade',
+              files = { 'src/parser.c' },
+              branch = 'main',
+            },
+            filetype = 'blade',
+          }
+        end,
+      },
+      'nvim-treesitter/nvim-treesitter-textobjects',
     },
-    'windwp/nvim-ts-autotag',
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  config = function()
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = {
-        'bash',
-        'c',
-        'html',
-        'lua',
-        'markdown',
-        'vim',
-        'vimdoc',
-        'css',
-        'javascript',
-        'json',
-        'typescript',
-        'vue',
-        'php',
-        'scss',
-        'yaml',
-        'gitignore',
-        'blade',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-
-      autotag = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
+    config = function()
+      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = {
+          'bash',
+          'c',
+          'html',
+          'lua',
+          'markdown',
+          'vim',
+          'vimdoc',
+          'css',
+          'javascript',
+          'json',
+          'typescript',
+          'vue',
+          'php',
+          'scss',
+          'yaml',
+          'gitignore',
+          'blade',
         },
-      },
-      textobjects = {
-        select = {
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+
+        -- There are additional nvim-treesitter modules that you can use to interact
+        -- with nvim-treesitter. You should go explore a few and see what interests you:
+        --
+        --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+        --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+        --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+
+        autotag = {
           enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        },
+        incremental_selection = {
+          enable = true,
           keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['aa'] = { query = '@parameter.outer', desc = 'Select outer part of a parameter/argument' },
-            ['ia'] = { query = '@parameter.inner', desc = 'Select inner part of a parameter/argument' },
-
-            ['ai'] = { query = '@conditional.outer', desc = 'Select outer part of a conditional' },
-            ['ii'] = { query = '@conditional.inner', desc = 'Select inner part of a conditional' },
-
-            ['al'] = { query = '@loop.outer', desc = 'Select outer part of a loop' },
-            ['il'] = { query = '@loop.inner', desc = 'Select inner part of a loop' },
-
-            ['af'] = { query = '@call.outer', desc = 'Select outer part of a function call' },
-            ['if'] = { query = '@call.inner', desc = 'Select inner part of a function call' },
-
-            ['am'] = { query = '@function.outer', desc = 'Select outer part of a method/function definition' },
-            ['im'] = { query = '@function.inner', desc = 'Select inner part of a method/function definition' },
-
-            ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class' },
-            ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class' },
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = false,
+            node_decremental = '<bs>',
           },
         },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']a'] = { query = '@parameter.outer', desc = 'Next parameter start' },
-            [']c'] = { query = '@class.outer', desc = 'Next class start' },
-            [']m'] = { query = '@function.outer', desc = 'Next method/function def start' },
-            [']f'] = { query = '@call.outer', desc = 'Next function call start' },
-            [']i'] = { query = '@conditional.outer', desc = 'Next conditional start' },
-            [']l'] = { query = '@loop.outer', desc = 'Next loop start' },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['aa'] = { query = '@parameter.outer', desc = 'Select outer part of a parameter/argument' },
+              ['ia'] = { query = '@parameter.inner', desc = 'Select inner part of a parameter/argument' },
+
+              ['ai'] = { query = '@conditional.outer', desc = 'Select outer part of a conditional' },
+              ['ii'] = { query = '@conditional.inner', desc = 'Select inner part of a conditional' },
+
+              ['al'] = { query = '@loop.outer', desc = 'Select outer part of a loop' },
+              ['il'] = { query = '@loop.inner', desc = 'Select inner part of a loop' },
+
+              ['af'] = { query = '@call.outer', desc = 'Select outer part of a function call' },
+              ['if'] = { query = '@call.inner', desc = 'Select inner part of a function call' },
+
+              ['am'] = { query = '@function.outer', desc = 'Select outer part of a method/function definition' },
+              ['im'] = { query = '@function.inner', desc = 'Select inner part of a method/function definition' },
+
+              ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class' },
+              ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class' },
+            },
           },
-          goto_next_end = {
-            [']F'] = { query = '@call.outer', desc = 'Next function call end' },
-            [']M'] = { query = '@function.outer', desc = 'Next method/function def end' },
-            [']C'] = { query = '@class.outer', desc = 'Next class end' },
-            [']A'] = { query = '@parameter.outer', desc = 'Next parameter end' },
-            [']I'] = { query = '@conditional.outer', desc = 'Next conditional end' },
-            [']L'] = { query = '@loop.outer', desc = 'Next loop end' },
-          },
-          goto_previous_start = {
-            ['[f'] = { query = '@call.outer', desc = 'Prev function call start' },
-            ['[m'] = { query = '@function.outer', desc = 'Prev method/function def start' },
-            ['[c'] = { query = '@class.outer', desc = 'Prev class start' },
-            ['[a'] = { query = '@parameter.outer', desc = 'Prev parameter start' },
-            ['[i'] = { query = '@conditional.outer', desc = 'Prev conditional start' },
-            ['[l'] = { query = '@loop.outer', desc = 'Prev loop start' },
-          },
-          goto_previous_end = {
-            ['[F'] = { query = '@call.outer', desc = 'Prev function call end' },
-            ['[M'] = { query = '@function.outer', desc = 'Prev method/function def end' },
-            ['[C'] = { query = '@class.outer', desc = 'Prev class end' },
-            ['[A'] = { query = '@parameter.outer', desc = 'Prev parameter end' },
-            ['[I'] = { query = '@conditional.outer', desc = 'Prev conditional end' },
-            ['[L'] = { query = '@loop.outer', desc = 'Prev loop end' },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']a'] = { query = '@parameter.outer', desc = 'Next parameter start' },
+              [']c'] = { query = '@class.outer', desc = 'Next class start' },
+              [']m'] = { query = '@function.outer', desc = 'Next method/function def start' },
+              [']f'] = { query = '@call.outer', desc = 'Next function call start' },
+              [']i'] = { query = '@conditional.outer', desc = 'Next conditional start' },
+              [']l'] = { query = '@loop.outer', desc = 'Next loop start' },
+            },
+            goto_next_end = {
+              [']F'] = { query = '@call.outer', desc = 'Next function call end' },
+              [']M'] = { query = '@function.outer', desc = 'Next method/function def end' },
+              [']C'] = { query = '@class.outer', desc = 'Next class end' },
+              [']A'] = { query = '@parameter.outer', desc = 'Next parameter end' },
+              [']I'] = { query = '@conditional.outer', desc = 'Next conditional end' },
+              [']L'] = { query = '@loop.outer', desc = 'Next loop end' },
+            },
+            goto_previous_start = {
+              ['[f'] = { query = '@call.outer', desc = 'Prev function call start' },
+              ['[m'] = { query = '@function.outer', desc = 'Prev method/function def start' },
+              ['[c'] = { query = '@class.outer', desc = 'Prev class start' },
+              ['[a'] = { query = '@parameter.outer', desc = 'Prev parameter start' },
+              ['[i'] = { query = '@conditional.outer', desc = 'Prev conditional start' },
+              ['[l'] = { query = '@loop.outer', desc = 'Prev loop start' },
+            },
+            goto_previous_end = {
+              ['[F'] = { query = '@call.outer', desc = 'Prev function call end' },
+              ['[M'] = { query = '@function.outer', desc = 'Prev method/function def end' },
+              ['[C'] = { query = '@class.outer', desc = 'Prev class end' },
+              ['[A'] = { query = '@parameter.outer', desc = 'Prev parameter end' },
+              ['[I'] = { query = '@conditional.outer', desc = 'Prev conditional end' },
+              ['[L'] = { query = '@loop.outer', desc = 'Prev loop end' },
+            },
           },
         },
-      },
-    }
+      }
 
-    local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
 
-    -- vim way: ; goes to the direction you were moving.
-    vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
-    vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
+      -- vim way: ; goes to the direction you were moving.
+      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
+      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
 
-    -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-    vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f)
-    vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F)
-    vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t)
-    vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T)
-  end,
+      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+      vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f)
+      vim.keymap.set({ 'n', 'x', 'o' }, 'F', ts_repeat_move.builtin_F)
+      vim.keymap.set({ 'n', 'x', 'o' }, 't', ts_repeat_move.builtin_t)
+      vim.keymap.set({ 'n', 'x', 'o' }, 'T', ts_repeat_move.builtin_T)
+    end,
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    after = { 'nvim-treesitter' },
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter' },
+    },
+    event = 'InsertEnter',
+  },
 }
