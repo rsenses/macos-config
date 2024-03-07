e39de87ae6200cf7c3c776f844b3fb6b35985a85
Rubén Silva <me@rsenses.com>
2024-03-07 18:22:41 +0100

    Elimina noice por errores varios

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -21,19 +21,23 @@ return {
     -- "gc" to comment visual regions/lines
     require('mini.comment').setup()
 
+    -- Notifications
+    require('mini.notify').setup()
+    vim.notify = require('mini.notify').make_notify()
+
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
-    -- local statusline = require 'mini.statusline'
-    -- statusline.setup()
+    local statusline = require 'mini.statusline'
+    statusline.setup()
 
     -- You can configure sections in the statusline by overriding their
     -- default behavior. For example, here we set the section for
     -- cursor location to LINE:COLUMN
     ---@diagnostic disable-next-line: duplicate-set-field
-    -- statusline.section_location = function()
-    --   return '%2l:%-2v'
-    -- end
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
