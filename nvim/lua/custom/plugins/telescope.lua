0be44d59fcfa4c236b4dac0e7ebf2f58686744e6
Rubén Silva <me@rsenses.com>
2024-03-07 13:07:50 +0100

    Anade neoclip

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -78,8 +78,9 @@ return {
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>st', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    vim.keymap.set('n', '<leader>st', builtin.builtin, { desc = '[S]earch select [T]elescope' })
     vim.keymap.set('n', '<leader>sc', ':TodoTelescope<cr>', { desc = '[S]earch TODO [C]omments' })
+    vim.keymap.set('n', '<leader>sy', ':Telescope neoclip<cr>', { desc = '[S]earch [Y]anks' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
