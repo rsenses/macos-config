5319e278cf57526a6ce663082ecd6c24f13470af
Rubén Silva <me@rsenses.com>
2024-03-11 17:39:19 +0100

    Update colorschema.lua

--- nvim/lua/custom/plugins/colorschema.lua
+++ nvim/lua/custom/plugins/colorschema.lua
@@ -4,10 +4,9 @@ return {
   -- change the command in the config to whatever the name of that colorscheme is
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-  'rebelot/kanagawa.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  'rebelot/kanagawa.nvim'
   priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
+  init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
