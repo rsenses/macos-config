87624c6ddd1fa9bce1d8a0fba8e0da64f84eb5ba
Rubén Silva <me@rsenses.com>
2024-03-12 08:59:58 +0100

    Anade mas colores por si me apetece cambiar

--- nvim/lua/custom/plugins/colorschema.lua
+++ nvim/lua/custom/plugins/colorschema.lua
@@ -1,18 +1,38 @@
 -- You can easily change to a different colorscheme.
 return {
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-  'rebelot/kanagawa.nvim',
-  priority = 1000, -- make sure to load this before all the other start plugins
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'kanagawa-wave'
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      integrations = {
+        mason = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+    'rebelot/kanagawa.nvim',
+    priority = 1000, -- make sure to load this before all the other start plugins
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'kanagawa-wave'
 
-    -- You can configure highlights by doing something like
-    vim.cmd.hi 'Comment gui=none'
-  end,
+      -- You can configure highlights by doing something like
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 }
