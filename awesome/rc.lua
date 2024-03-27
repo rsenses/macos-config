4a409b8231d44fd7449d2897a297a73875de5754
Rubén Silva <me@rsenses.com>
2024-03-27 14:57:11 +0100

    Disable snap edges

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -289,6 +289,7 @@ root.buttons(gears.table.join(
     awful.button({}, 4, awful.tag.viewnext),
     awful.button({}, 5, awful.tag.viewprev)
 ))
+awful.mouse.snap.edge_enabled = false
 -- }}}
 
 -- {{{ Key bindings
