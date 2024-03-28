ae57f0790276197b7d603260dcbb942787ce0306
Rubén Silva <me@rsenses.com>
2024-03-28 11:06:30 +0100

    Comenta nuevos widgets momentaneamente

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -307,10 +307,10 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.layout.margin(wibox.widget.systray(), 3, 3, 3, 3),
-            brightness_widget,
-            volume_widget({
-                widget_type = "icon_and_text",
-            }),
+            -- brightness_widget,
+            -- volume_widget({
+            --     widget_type = "icon_and_text",
+            -- }),
             mytextclock,
             -- s.mylayoutbox,
         },
