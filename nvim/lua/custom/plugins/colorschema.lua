a6eed8f7bd75fae5b0e0fae9ac670c3276d271db
Rubén Silva <me@rsenses.com>
2024-04-20 12:24:19 +0200

    Mejora colores en tmux

--- nvim/lua/custom/plugins/colorschema.lua
+++ nvim/lua/custom/plugins/colorschema.lua
@@ -26,6 +26,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.o.termguicolors = true
       vim.cmd.colorscheme 'kanagawa-wave'
 
       -- You can configure highlights by doing something like
