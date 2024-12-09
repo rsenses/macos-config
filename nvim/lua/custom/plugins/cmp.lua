e5e13978da66d19786cefcb64ea9562daa0d41f8
Rubén Silva <me@rsenses.com>
2024-12-09 17:14:29 +0100

    Simplifica la configuración de CMP

--- nvim/lua/custom/plugins/cmp.lua
+++ nvim/lua/custom/plugins/cmp.lua
@@ -142,31 +142,31 @@ return {
         { name = 'path' },
         { name = 'buffer' },
       },
-      sorting = {
-        comparators = {
-          cmp.config.compare.exact,
-          cmp.config.compare.score,
-          cmp.config.compare.recently_used,
-          cmp.config.compare.locality,
-          cmp.config.compare.offset,
-
-          -- copied from cmp-under, but I don't think I need the plugin for this.
-          -- I might add some more of my own.
-          function(entry1, entry2)
-            local _, entry1_under = entry1.completion_item.label:find '^_+'
-            local _, entry2_under = entry2.completion_item.label:find '^_+'
-            entry1_under = entry1_under or 0
-            entry2_under = entry2_under or 0
-            if entry1_under > entry2_under then
-              return false
-            elseif entry1_under < entry2_under then
-              return true
-            end
-          end,
-
-          cmp.config.compare.kind,
-        },
-      },
+      -- sorting = {
+      --   comparators = {
+      --     cmp.config.compare.exact,
+      --     cmp.config.compare.score,
+      --     cmp.config.compare.recently_used,
+      --     cmp.config.compare.locality,
+      --     cmp.config.compare.offset,
+      --
+      --     -- copied from cmp-under, but I don't think I need the plugin for this.
+      --     -- I might add some more of my own.
+      --     function(entry1, entry2)
+      --       local _, entry1_under = entry1.completion_item.label:find '^_+'
+      --       local _, entry2_under = entry2.completion_item.label:find '^_+'
+      --       entry1_under = entry1_under or 0
+      --       entry2_under = entry2_under or 0
+      --       if entry1_under > entry2_under then
+      --         return false
+      --       elseif entry1_under < entry2_under then
+      --         return true
+      --       end
+      --     end,
+      --
+      --     cmp.config.compare.kind,
+      --   },
+      -- },
     }
 
     require('nvim-cmp-laravel').setup()
