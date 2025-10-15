1cef5624aaffd31a633edcbe736212b5fc27dc76
Rubén Silva <me@rsenses.com>
2025-10-15 15:51:50 +0200

    refactor: cleanup

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -61,8 +61,8 @@ return {
 
       require('ts_context_commentstring').setup {
         enable_autocmd = false,
-        custom_calculation = function(_, language_tree)
-          if vim.bo.filetype == 'blade' and language_tree._lang ~= 'javascript' and language_tree._lang ~= 'php' then
+        custom_calculation = function(_, _)
+          if vim.bo.filetype == 'blade' then
             return '{{-- %s --}}'
           end
         end,
