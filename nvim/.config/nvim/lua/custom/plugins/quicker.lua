328e8a1a664bde990c731db04c544dc946dea9bc
Rubén Silva <me@rsenses.com>
2025-11-21 09:59:42 +0100

    feat: adds quicker nvim

--- nvim/.config/nvim/lua/custom/plugins/quicker.lua
+++ nvim/.config/nvim/lua/custom/plugins/quicker.lua
@@ -3,5 +3,22 @@ return {
   ft = 'qf',
   ---@module "quicker"
   ---@type quicker.SetupOptions
-  opts = {},
+  opts = {
+    keys = {
+      {
+        '>',
+        function()
+          require('quicker').expand { before = 2, after = 2, add_to_existing = true }
+        end,
+        desc = 'Expand quickfix context',
+      },
+      {
+        '<',
+        function()
+          require('quicker').collapse()
+        end,
+        desc = 'Collapse quickfix context',
+      },
+    },
+  },
 }
